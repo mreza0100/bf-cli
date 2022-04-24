7bd34acd8a2cfe51ba6493b6a9832edfcdcc3d88
mohammad reza khosravi <mrez9090@gmail.com>
2022-04-25 03:44:01 +0430

    change version

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ var app = cli.NewApp()
 func main() {
 	app.Name = "brainfuck-cli"
 	app.Usage = "brainfuck-cli"
-	app.Version = "0.1.7"
+	app.Version = "0.1.8"
 	app.Author = "M.Reza khosravi"
 
 	actions := new(Actions)
